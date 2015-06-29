858977f5bd9d89bba99ed1a80e8875cb91ce8909
Stephen Gaito <stephen@perceptisys.co.uk>
2015-06-29 16:52:52 +0100

    added IndexedBlockAllocators

---
+++ lib/cUtils/indexedBlockAllocator.h
@@ -0,0 +1,86 @@
+#ifndef INDEXED_BLOCK_ALLOCATOR_H
+#define INDEXED_BLOCK_ALLOCATOR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "cUtils/blockAllocator.h"
+
+/// \brief The IndexedBlockAllocator class holds the information
+/// required to allocate multiple blocks of related (sub)structures
+/// which are allocated at continquous indexes.
+class IndexedBlockAllocator : public BlockAllocator {
+
+  public:
+
+    /// \brief An invariant which should ALWAYS be true for any
+    /// instance of a IndexedBlockAllocator class.
+    ///
+    /// Throws an AssertionFailure with a brief description of any
+    /// inconsistencies discovered.
+    bool invariant(void) const {
+      if (blockSize != (1<<bitShift)*itemSize) {
+        throw AssertionFailure("incorrect block size");
+      }
+      return BlockAllocator::invariant();
+    }
+
+    /// \brief Create a new indexed block allocator which allocates items
+    /// of a given anItemSize.
+    ///
+    /// ABitSize determines the balance between block size and the
+    /// number of blocks.
+    IndexedBlockAllocator(size_t anItemSize,
+                          size_t aBitShift = 5)
+                        : BlockAllocator( (1<<aBitShift)*anItemSize ){
+      itemSize = anItemSize;
+      bitShift = aBitShift;
+      ASSERT(invariant());
+    }
+
+    /// \brief Allocate an indexed new (sub)structure.
+    ///
+    /// The indexes returns are contiquous.
+    size_t allocateNewStructure(void) {
+      ASSERT(invariant());
+      char * itemPtr =  BlockAllocator::allocateNewStructure(itemSize);
+      size_t blockNum = blocks.getNumItems() - 1;
+      return (blockNum << bitShift) + (itemPtr - blocks.getTop())/itemSize;
+    }
+
+    /// \brief Compute the char* pointer corresponding to this
+    /// itemNumber.
+    char *getItemPtr(size_t itemNum) {
+      ASSERT(invariant());
+      size_t blockNum = itemNum >> bitShift;
+      if (blocks.getNumItems() <= blockNum) return NULL;
+      char *blockPtr = blocks.getItem(blockNum, NULL);
+      if (!blockPtr) return NULL;
+      size_t itemMask = ~((~0L)<<bitShift);
+      char *itemPtr = blockPtr + (itemNum&itemMask)*itemSize;
+      if (curAllocationByte <= itemPtr) return NULL;
+      return itemPtr;
+    }
+
+  protected:
+
+    /// \brief Override the BlockAllocator::allocateNewStructure to
+    /// prevent its use.
+    size_t allocateNewStructure(size_t structureSize) {
+      ASSERT(invariant());
+      return 0;
+    }
+
+    /// \brief The size of each indexed item to be block allocated by
+    /// this class.
+    size_t itemSize;
+
+    /// \brief The bitShift represents the power of two number of items
+    /// in a given block.
+    ///
+    /// We use a power of two to keep the computation of index->pointer
+    /// simple.
+    size_t bitShift;
+};
+
+#endif
---
+++ tests/indexedBlockAllocator.cpp
@@ -0,0 +1,111 @@
+#include <bandit/bandit.h>
+using namespace bandit;
+
+#include <string.h>
+#include <stdio.h>
+#include <exception>
+
+#ifndef protected
+#define protected public
+#endif
+
+#include <stdio.h>
+#include <cUtils/indexedBlockAllocator.h>
+
+go_bandit([](){
+
+  printf("\n----------------------------------\n");
+  printf(  "indexedBlockAllocator\n");
+  printf(  "IndexedBlockAllocator = %zu bytes (%zu bits)\n", sizeof(IndexedBlockAllocator), sizeof(IndexedBlockAllocator)*8);
+  printf(  "----------------------------------\n");
+
+  /// \brief We test the correctness of the C-based BlockAllocator structure.
+  ///
+  describe("IndexedBlockAllocator", [](){
+
+    it("should create an IndexedBlockAllocator", [](){
+      IndexedBlockAllocator *iba = new IndexedBlockAllocator(11, 4);
+      AssertThat(iba, Is().Not().EqualTo((void*)0));
+      AssertThat(iba->itemSize, Equals(11));
+      AssertThat(iba->bitShift, Equals(4));
+      delete iba;
+    });
+
+    it("can allocate a new item", [](){
+      IndexedBlockAllocator *iba = new IndexedBlockAllocator(11, 4);
+      AssertThat(iba, Is().Not().EqualTo((void*)0));
+      size_t iba0 = iba->allocateNewStructure();
+      size_t iba1 = iba->allocateNewStructure();
+      AssertThat(iba0, Equals(0));
+      AssertThat(iba1, Equals(1));
+      AssertThat(iba->getItemPtr(iba0), Equals(iba->blocks.getTop()));
+      AssertThat(iba->getItemPtr(iba1), Equals(iba->blocks.getTop()+11));
+      delete iba;
+    });
+
+    it("can allocate lots of new items", [](){
+      IndexedBlockAllocator *iba = new IndexedBlockAllocator(11, 4);
+      AssertThat(iba, Is().Not().EqualTo((void*)0));
+      AssertThat(iba->blocks.getNumItems(), Equals(0));
+      for (size_t i = 0; i < 1<<5; i++) {
+        size_t ibaItem = iba->allocateNewStructure();
+        AssertThat(ibaItem, Equals(i));
+      }
+      AssertThat(iba->blocks.getNumItems(), Equals(2));
+      size_t ibaItem0 = iba->allocateNewStructure();
+      AssertThat(ibaItem0, Equals(1<<5));
+      AssertThat(iba->blocks.getNumItems(), Equals(3));
+      AssertThat(iba->getItemPtr(ibaItem0), Equals(iba->blocks.getTop()));
+      size_t ibaItem1 = iba->allocateNewStructure();
+      AssertThat(ibaItem1, Equals((1<<5)+1));
+      AssertThat(iba->blocks.getNumItems(), Equals(3));
+      AssertThat(iba->getItemPtr(ibaItem1), Equals(iba->blocks.getTop()+11));
+      delete iba;
+    });
+
+    it("can allocate lots of new items, clear them and then allocate some more", [](){
+      IndexedBlockAllocator *iba = new IndexedBlockAllocator(11, 4);
+      AssertThat(iba, Is().Not().EqualTo((void*)0));
+      AssertThat(iba->blocks.getNumItems(), Equals(0));
+      for (size_t i = 0; i < 1<<5; i++) {
+        size_t ibaItem = iba->allocateNewStructure();
+        AssertThat(ibaItem, Equals(i));
+      }
+      AssertThat(iba->blocks.getNumItems(), Equals(2));
+      size_t ibaItem0 = iba->allocateNewStructure();
+      AssertThat(ibaItem0, Equals(1<<5));
+      AssertThat(iba->blocks.getNumItems(), Equals(3));
+      AssertThat(iba->getItemPtr(ibaItem0), Equals(iba->blocks.getTop()));
+      size_t ibaItem1 = iba->allocateNewStructure();
+      AssertThat(ibaItem1, Equals((1<<5)+1));
+      AssertThat(iba->blocks.getNumItems(), Equals(3));
+      AssertThat(iba->getItemPtr(ibaItem1), Equals(iba->blocks.getTop()+11));
+      iba->clearBlocks();
+      AssertThat(iba->blocks.getNumItems(), Equals(0));
+      for (size_t i = 0; i < 1<<5; i++) {
+        size_t ibaItem = iba->allocateNewStructure();
+        AssertThat(ibaItem, Equals(i));
+      }
+      AssertThat(iba->blocks.getNumItems(), Equals(2));
+      ibaItem0 = iba->allocateNewStructure();
+      AssertThat(ibaItem0, Equals(1<<5));
+      AssertThat(iba->blocks.getNumItems(), Equals(3));
+      AssertThat(iba->getItemPtr(ibaItem0), Equals(iba->blocks.getTop()));
+      ibaItem1 = iba->allocateNewStructure();
+      AssertThat(ibaItem1, Equals((1<<5)+1));
+      AssertThat(iba->blocks.getNumItems(), Equals(3));
+      AssertThat(iba->getItemPtr(ibaItem1), Equals(iba->blocks.getTop()+11));
+      delete iba;
+    });
+
+    it("should NOT allow the use of allocateNewStructure(size_t)", [](){
+      IndexedBlockAllocator *iba = new IndexedBlockAllocator(11, 4);
+      AssertThat(iba, Is().Not().EqualTo((void*)0));
+      for (size_t i = 0; i < 10; i++) {
+        size_t ibaItem0 = iba->allocateNewStructure(5);
+        AssertThat(ibaItem0, Equals(0));
+      }
+    });
+
+  }); // indexedBlockAllocator
+}); // go_bandit
